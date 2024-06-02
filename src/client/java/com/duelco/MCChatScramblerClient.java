72902a78ac1f9698243b2d1670dd6628c09d0573
DuelistHeart <AshleyHarper2024@gmail.com>
2024-06-02 13:22:43 -0400

    Initial Commit

---
+++ src/client/java/com/duelco/MCChatScramblerClient.java
@@ -0,0 +1,10 @@
+package com.duelco;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCChatScramblerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
